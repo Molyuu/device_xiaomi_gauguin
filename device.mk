038d0dc3dfbbbc2783b5874d35a8591bb33d3a76
kawaaii <kawaaii@nocturn9x.space>
2022-08-27 13:23:05 +0545

    gauguin: rootdir: fstabs: Migrate to fstab.qcom
    Change-Id: I7fb4030f46932936e2b295f2dfe7c0c14f9b3409

--- device.mk
+++ device.mk
@@ -256,10 +256,8 @@ PRODUCT_PACKAGES += \
 
 # Init scripts
 PRODUCT_PACKAGES += \
-    fstab.default \
-    fstab.default.ramdisk \
-    fstab.emmc \
-    fstab.emmc.ramdisk \
+    fstab.qcom \
+    fstab.qcom.ramdisk \
     init.class_main.sh \
     init.mdm.sh \
     init.qcom.class_core.sh \
