449d5bf19391e427887738da4cb3041a2b2475fb
LynnrinChan <lynnrin@lynnrin.moe>
2023-01-01 23:33:28 +0800

    gauguin: sepolicy: Switch to sepolicy_vndr-legacy-um
    Change-Id: Ifda2fe20a244f9eee7f7628daa23b107b45ea483

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,7 +166,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-05-01
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SELINUX_IGNORE_NEVERALLOW := true
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
